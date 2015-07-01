de8a68e122da198536c1d5825fce6b0b6fc4f753
g13960ym <g13960ym@m39902.tsuda.ac.jp>
2015-07-01 11:19:33 +0900

    ログイン前の画面までほちゃんと表示できるようになりました。

--- src/jp/tsuda/LoginServlet.java
+++ src/jp/tsuda/LoginServlet.java
@@ -4,48 +4,46 @@
  */
 package jp.tsuda;
 
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
 
-import javax.jdo.*;
-import javax.servlet.http.*;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jp.tsuda.Login;
+import jp.tsuda.PMF;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 
 
 @SuppressWarnings("serial")
 public class LoginServlet extends HttpServlet {
 
-    
-    protected void doGet(HttpServletRequest req,
-            HttpServletResponse resp)
-            throws IOException {
-    	PersistenceManagerFactory factory = PMF.get();
-        PersistenceManager manager = factory.getPersistenceManager();
-        resp.setCharacterEncoding("UTF-8");
-        resp.setContentType("text/html");
-        req.setCharacterEncoding("utf-8");
-        String param1 = req.getParameter("id");
-        PrintWriter out = resp.getWriter();
-        List<Account> list = null;
-        if (param1 == null || param1 ==""){
-            String query = "select from " + Account.class.getName();
-            try {
-                list = (List<Account>)manager.newQuery(query).execute();
-            } catch(JDOObjectNotFoundException e){}
-        } else {
-            try {
-                Account data = (Account)manager.getObjectById(Account.class,Long.parseLong(param1));
-                list = new ArrayList();
-                list.add(data);
-            } catch(JDOObjectNotFoundException e){}
-        }
-        String res = "[";
-        if (list != null){
-            for(Account data:list){
-                res += "{id:" + data.getUserId() + ",pass:'" + data.getPass() + "'},";
-            }
-        }
-        res += "]";
-        out.println(res);
-        manager.close();
-    }
+	 @Override
+	    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	            throws IOException {
+	        UserService userService = UserServiceFactory.getUserService();
+
+	        String thisURL = req.getRequestURI();
+
+	        resp.setContentType("text/html");
+	        if (req.getUserPrincipal() != null) {
+	            resp.getWriter().println("<p>Hello, " +
+	                                     req.getUserPrincipal().getName() +
+	                                     "!  You can <a href=\"" +
+	                                     userService.createLogoutURL(thisURL) +
+	                                     "\">sign out</a>.</p>");
+	        } else {
+	            resp.getWriter().println("<p>Please <a href=\"" +
+	                                     userService.createLoginURL(thisURL) +
+	                                     "\">sign in</a>.</p>");
+	        }
+	    }
 }
+	 
+
+	
--- src/jp/tsuda/WelcomeServlet.java
+++ src/jp/tsuda/WelcomeServlet.java
@@ -5,6 +5,9 @@ import java.io.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
 
 
 
@@ -12,11 +15,24 @@ import javax.servlet.http.*;
 public class WelcomeServlet extends HttpServlet {
 
 	@Override
-    protected void doGet(HttpServletRequest req,
-            HttpServletResponse resp)
-            throws ServletException, IOException {
-        resp.setContentType("text/plain");
-        resp.getWriter().println("please wait...");
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        UserService userService = UserServiceFactory.getUserService();
+
+        String thisURL = req.getRequestURI();
+
+        resp.setContentType("text/html");
+        if (req.getUserPrincipal() != null) {
+            resp.getWriter().println("<p>Hello, " +
+                                     req.getUserPrincipal().getName() +
+                                     "!  You can <a href=\"" +
+                                     userService.createLogoutURL(thisURL) +
+                                     "\">sign out</a>.</p>");
+        } else {
+            resp.getWriter().println("<p>Please <a href=\"" +
+                                     userService.createLoginURL(thisURL) +
+                                     "\">sign in</a>.</p>");
+        }
     }
  
     @Override
